208926edd18931d62f6d5b6a30f48a5710f58ef6
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2021-08-18 22:37:55 -0300

    UseEffect e UseRef

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,13 +1,65 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectioTitle from "../../components/layout/SectionTitle"
+
+
+function calcFatorial(num) {
+    
+    try {
+        const n = parseInt(num)
+        if (n < 0) return -1
+        if (n === 0) return 1
+
+        return calcFatorial(n - 1) * n
+    } catch (error) {
+        return -2
+    }
+   
+    
+}
 
 const UseEffect = (props) => {
+    const [number, setNamber] = useState(1)
+    const [fatorial, setFatorial] = useState(1)
+
+    useEffect(function () {
+        setFatorial(calcFatorial(number))
+        
+    }, [number])
+
+
+const [status, setStatus] = useState("Impar")
+useEffect(function () {
+    setStatus(number % 2===0 ? "Par" : "Impar" )
+    
+})
+   
+
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectioTitle title="Exercicio #01"></SectioTitle >
+            <div className="center">
+                <span className="text">Fatorial</span>
+                <span className="text red">{fatorial === -1? "Não existe": fatorial}</span>
+                <input type="number" className="input" 
+                    value={number} onChange={e => setNamber(e.target.value)}/>
+            </div>
+
+            <SectioTitle title="Exercicio #02"></SectioTitle >
+            <div className="center">
+                <span className="text"> Status: </span>
+                <span className="text red">{status}</span>
+               
+
+            </div>
+
         </div>
     )
 }
--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -1,13 +1,74 @@
+
 import React from 'react'
+import { useEffect } from 'react'
+import { useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+const merge = function(s1, s2) {
+    return [...s1].map(function (e, i) {
+        return `${e}${s2[i] || ""}` 
+        
+    }).join("")
+}
+
 
 const UseRef = (props) => {
+
+
+    const [value1, setValue] = useState("")
+    const [value2, setValue2] = useState("")
+    const count = useRef(0)
+    const myInput1 = useRef(0)
+    const myInput2 = useRef(0)
+
+
+    useEffect(function () {
+        count.current = count.current + 1
+        myInput2.current.focus()
+        
+    }, [value1])
+
+    useEffect(function () {
+        count.current = count.current + 1
+        myInput1.current.focus()
+
+    }, [value2])
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+
+            <SectionTitle title="Exercicio #01"/>
+
+            <div className="center">
+                <div>
+                    <span className="text">Valor: </span>
+                    <span className="text">{merge(value1, value2)} [</span>
+                    <span className="text">{count.current}</span>
+                    <span className="text">]</span>
+                </div>
+                <input type="text" className="input" 
+                ref={myInput1}
+                value={value1} onChange={e=> setValue(e.target.value)}/>
+
+            </div>
+
+
+            <SectionTitle title="Exercicio #02" />
+
+            <div className="center">
+                <div>
+                   
+                </div>
+                <input type="text" className="input"
+                ref={myInput2}
+                    value={value2} onChange={e => setValue2(e.target.value)} />
+
+            </div>
         </div>
     )
 }
